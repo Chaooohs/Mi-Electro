92ac1effec66bf9f2084691d3855dc20ec86b5a7
Chaooohs <01nikka01@gmail.com>
2023-07-09 20:03:49 +0300

    adf

---
+++ js/buick.js
@@ -0,0 +1,28 @@
+//==================================
+
+// const radio = document.getElementsByName('buick');
+const radio = document.querySelectorAll('.buick__input');
+const buick = document.querySelector('#buick');
+
+
+// for (let i = 0; i<radio.length; i++) {
+//   radio[i].onchange = testRadio;
+// }
+
+for (colorBuickValue of radio) {
+  colorBuickValue.onchange = testRadio;
+}
+
+function testRadio() {
+  // console.log(this.value);
+  if (this.value === 'black') {
+    buick.classList.add('buick_black')
+    buick.classList.remove('buick_white')
+  } 
+  else {
+    buick.classList.add('buick_white')
+    buick.classList.remove('buick_black')
+  }
+}
+
+
