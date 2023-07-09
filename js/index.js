75328c16ccb7e15c3ee871eb2a74118ddf68203d
Chaooohs <01nikka01@gmail.com>
2023-07-09 08:01:04 +0300

    adsf

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const white = document.getElementById('white');
 const black = document.getElementById('black');
 const page = document.getElementById('page');
 const widthDesktop = window.matchMedia("(min-width: 993px)");
-const widthLaptop = window.matchMedia("(min-width:641px)");
+const widthLaptop = window.matchMedia("(max-width: 992px)");
 const widthMobile = window.matchMedia("(max-width: 640px)");
 
 
@@ -17,19 +17,25 @@ const widthMobile = window.matchMedia("(max-width: 640px)");
 //     page.style.backgroundImage = "url('../img/buick/buick-black-laptop.png')";
 // });
 
-white.addEventListener('click', function(widthMobile){
-    page.style.backgroundImage = "url('../img/buick/buick-white-mobile.png')";
-});
+// white.addEventListener('click', function () {
+//     page.style.backgroundImage = "url('../img/buick/buick-white-mobile.png')";
+// });
 
-black.addEventListener('click', function(widthMobile){
-    page.style.backgroundImage = "url('../img/buick/buick-black-mobile.png')";
-});
+// black.addEventListener('click', function () {
+//     page.style.backgroundImage = "url('../img/buick/buick-black-mobile.png')";
+// });
 
 // desktop -----------------------------------------------------
 
-function buickDesktop (widthDesktop) {
+function buickDesktop(widthDesktop) {
     if (widthDesktop.matches) {
+        white.addEventListener('click', function () {
+            page.style.backgroundImage = "url('../img/buick/buick-white-desktop.png')";
+        });
+
+        black.addEventListener('click', function () {
             page.style.backgroundImage = "url('../img/buick/buick-black-desktop.png')";
+        });
     }
 }
 widthDesktop.addListener(buickDesktop);
@@ -37,9 +43,15 @@ buickDesktop(widthDesktop);
 
 // laptop -------------------------------------------------------
 
-function buickLaptop (widthLaptop) {
+function buickLaptop(widthLaptop) {
     if (widthLaptop.matches) {
+        white.addEventListener('click', function () {
+            page.style.backgroundImage = "url('../img/buick/buick-white-laptop.png')";
+        });
+
+        black.addEventListener('click', function () {
             page.style.backgroundImage = "url('../img/buick/buick-black-laptop.png')";
+        });
     }
 }
 widthLaptop.addListener(buickLaptop);
@@ -47,9 +59,15 @@ buickLaptop(widthLaptop);
 
 // mobile ------------------------------------------------------
 
-function buickMobile (widthMobile) {
+function buickMobile(widthMobile) {
     if (widthMobile.matches) {
+        white.addEventListener('click', function () {
+            page.style.backgroundImage = "url('../img/buick/buick-white-mobile.png')";
+        });
+        
+        black.addEventListener('click', function () {
             page.style.backgroundImage = "url('../img/buick/buick-black-mobile.png')";
+        });
     }
 }
 widthMobile.addListener(buickMobile);
