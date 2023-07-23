25b23a2689ea31cc9ad973564b31ab87eb32ea41
Chaooohs <01nikka01@gmail.com>
2023-07-24 00:48:25 +0300

    adf

--- js/buick.js
+++ js/buick.js
@@ -16,68 +16,27 @@ $("#whiteBuick").addEventListener("click", function () {
   added($("#whiteBuick"));
 });
 
+$("#blackBuickFooter").addEventListener("click", function () {
+  clear($("#whiteBuickFooter"));
+  added($("#blackBuickFooter"));
+  $("#buickFooterImageDesk").srcset="/img/buick/buick-footer-black-desk.png"
+  $("#buickFooterImageLapt").srcset="/img/buick/buick-footer-black-lapt.png"
+  $("#buickFooterImageMob").srcset="/img/buick/buick-footer-black-mob.png"
+});
+
+$("#whiteBuickFooter").addEventListener("click", function () {
+  clear($("#blackBuickFooter"));
+  added($("#whiteBuickFooter"));
+  $("#buickFooterImageDesk").srcset="/img/buick/buick-footer-white-desk.png"
+  $("#buickFooterImageLapt").srcset="/img/buick/buick-footer-white-lapt.png"
+  $("#buickFooterImageMob").srcset="/img/buick/buick-footer-white-mob.png"
+});
+
 
 const added = (el) => el.classList.add("buick__button_active");
-
 const clear = (el) => el.classList.remove("buick__button_active");
 
 
-//---------------------------------------------------------------------
+//===============================================================================================
 
-const deskWidth = window.matchMedia("(min-width: 993px)");
-function desktopScreen(deskWidth) {
-  if (deskWidth.matches) {
-    $("#blackBuickFooter").onclick = () => {
-      clear($("#whiteBuickFooter"));
-      added($("#blackBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-black-desk.png";
-    };
-    $("#whiteBuickFooter").onclick = () => {
-      clear($("#blackBuickFooter"));
-      added($("#whiteBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-white-desk.png";
-    };
-  }
-  else {
-    clear($("#whiteBuickFooter"));
-    added($("#blackBuickFooter"));
-    $("#buickFooterImage").src = "/img/buick/buick-footer-black-lapt.png";
-  }
-}
-deskWidth.addListener(desktopScreen);
-desktopScreen(deskWidth);
-
-//--------------------------------------------------------------------
-
-const laptopWidth = window.matchMedia("(min-width: 641px)");
-function laptopScreen(laptopWidth) {
-  if (laptopWidth.matches) {
-    $("#blackBuickFooter").onclick = () => {
-      clear($("#whiteBuickFooter"));
-      added($("#blackBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-black-lapt.png";
-    };
-    $("#whiteBuickFooter").onclick = () => {
-      clear($("#blackBuickFooter"));
-      added($("#whiteBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-white-lapt.png";
-    };
-  }
-  else {
-    $("#blackBuickFooter").onclick = () => {
-      clear($("#whiteBuickFooter"));
-      added($("#blackBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-black-mob.png";
-    };
-    $("#whiteBuickFooter").onclick = () => {
-      clear($("#blackBuickFooter"));
-      added($("#whiteBuickFooter"));
-      $("#buickFooterImage").src = "/img/buick/buick-footer-white-mob.png";
-    };
-  }
-}
-laptopWidth.addListener(laptopScreen);
-laptopScreen(laptopWidth);
-
-//--------------------------------------------------------------------
 
