2bcc37002489e1ffce1c314758dfb553c91f0857
Chaooohs <01nikka01@gmail.com>
2023-07-23 18:44:55 +0300

    afd

--- js/buick.js
+++ js/buick.js
@@ -3,18 +3,26 @@
 const $ = (selector) => document.querySelector(selector);
 
 $("#blackBuick").onclick = () => {
+  $("#whiteBuick").classList.remove("buick__button_active")
+  $("#blackBuick").classList.add("buick__button_active")
   buick.classList.add("buick_black");
   buick.classList.remove("buick_white");
 };
 $("#whiteBuick").onclick = () => {
+  $("#blackBuick").classList.remove("buick__button_active")
+  $("#whiteBuick").classList.add("buick__button_active")
   buick.classList.add("buick_white");
   buick.classList.remove("buick_black");
 };
 
 $("#blackBuickFooter").onclick = () => {
+  $("#whiteBuickFooter").classList.remove("buick__button_active")
+  $("#blackBuickFooter").classList.add("buick__button_active")
   $("#buickFooterBlackMob").src = "./img/buick/buick-footer-blakc-mob.png";
 };
 
 $("#whiteBuickFooter").onclick = () => {
+  $("#blackBuickFooter").classList.remove("buick__button_active")
+  $("#whiteBuickFooter").classList.add("buick__button_active")
   $("#buickFooterBlackMob").src = "./img/buick/buick-footer-white-mob.png";
 };
