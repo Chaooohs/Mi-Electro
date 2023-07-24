7745e286bb462cb4fc5dd55b45114eb791d5aaef
Chaooohs <01nikka01@gmail.com>
2023-07-24 23:24:19 +0300

    dsf

--- js/style.js
+++ js/style.js
@@ -5,12 +5,14 @@ const el = (selector) => document.querySelector(selector);
 
 el("#listBtn").onclick = () => el("#list").classList.toggle("hide-list");
 
+
 //  mobile menu
 const menu = () => el("#mobMenu").classList.toggle("active");
 const scroll = () => el("body").classList.toggle("no-scroll");
 
 el("#burgerBtn").addEventListener("click", menu);
 el("#burgerBtn").addEventListener("click", scroll);
+
 
 // closing the mobile menu after clicking on a link
 let [...link] = document.querySelectorAll("#mobMenu a");
@@ -20,3 +22,5 @@ link.forEach((item) => {
     el("body").classList.remove("no-scroll");
   });
 });
+
+
