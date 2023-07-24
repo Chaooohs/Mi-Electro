4b173c895bd0ac761d70f14fff48b999ccfe1fa9
Chaooohs <01nikka01@gmail.com>
2023-07-24 21:33:38 +0300

    adf

--- js/style.js
+++ js/style.js
@@ -1,5 +1,15 @@
 //==============================================================
 
+// contents of delivery
 const el = (selector) => document.querySelector(selector);
 
 el("#listBtn").onclick = () => el("#list").classList.toggle("hide-list");
+
+
+// navbar mobile
+const menu = () => el('#mobMenu').classList.toggle('active')
+const scroll = () => el('body').classList.toggle('no-scroll')
+
+
+el('#burgerBtn').addEventListener('click', menu)
+el('#burgerBtn').addEventListener('click', scroll)
