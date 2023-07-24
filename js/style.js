be2a7f21855d41db558259ab03e579abfa688752
Chaooohs <01nikka01@gmail.com>
2023-07-24 23:15:21 +0300

    adsf

--- js/style.js
+++ js/style.js
@@ -5,22 +5,18 @@ const el = (selector) => document.querySelector(selector);
 
 el("#listBtn").onclick = () => el("#list").classList.toggle("hide-list");
 
+//  mobile menu
+const menu = () => el("#mobMenu").classList.toggle("active");
+const scroll = () => el("body").classList.toggle("no-scroll");
 
-// navbar mobile
-const menu = () => el('#mobMenu').classList.toggle('active')
-const scroll = () => el('body').classList.toggle('no-scroll')
+el("#burgerBtn").addEventListener("click", menu);
+el("#burgerBtn").addEventListener("click", scroll);
 
-
-el('#burgerBtn').addEventListener('click', menu)
-el('#burgerBtn').addEventListener('click', scroll)
-
-let [...link] = document.querySelectorAll('#mobMenu a')
-link.forEach( function (item) {
-  item.addEventListener('click', function () {
-    el('#mobMenu').classList.remove('active')
-    el('body').classList.remove('no-scroll')
-  })
-})
-
-
-
+// closing the mobile menu after clicking on a link
+let [...link] = document.querySelectorAll("#mobMenu a");
+link.forEach((item) => {
+  item.addEventListener("click", function () {
+    el("#mobMenu").classList.remove("active");
+    el("body").classList.remove("no-scroll");
+  });
+});
