01b0fee732179d05cee0b4ec8d01dfa3103cbb75
Chaooohs <01nikka01@gmail.com>
2023-07-24 22:34:01 +0300

    adsf

--- js/style.js
+++ js/style.js
@@ -13,3 +13,14 @@ const scroll = () => el('body').classList.toggle('no-scroll')
 
 el('#burgerBtn').addEventListener('click', menu)
 el('#burgerBtn').addEventListener('click', scroll)
+
+let [...link] = document.querySelectorAll('#mobMenu a')
+link.forEach( function (item) {
+  item.addEventListener('click', function () {
+    el('#mobMenu').classList.remove('active')
+    el('body').classList.remove('no-scroll')
+  })
+})
+
+
+
