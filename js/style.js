b68ae08a50a8705a4ae66ee6f7a32946475863b8
Chaooohs <01nikka01@gmail.com>
2023-07-25 19:23:12 +0300

    adsf

--- js/style.js
+++ js/style.js
@@ -7,20 +7,27 @@ el("#listBtn").onclick = () => el("#list").classList.toggle("hide-list");
 
 
 //  mobile menu
-const menu = () => el("#mobMenu").classList.toggle("active");
+const menu = function () {
+  el('#underLay').classList.toggle('active');
+  el('#mobMenu').classList.toggle('active');
+}
+
 const scroll = () => el("body").classList.toggle("no-scroll");
 
-el("#burgerBtn").addEventListener("click", menu);
-el("#burgerBtn").addEventListener("click", scroll);
+el('#burgerBtn').addEventListener('click', menu);
+el('#burgerBtn').addEventListener('click', scroll);
+
+el('#underLay').addEventListener('click', function () {
+  menu()
+  scroll()
+})
 
 
 // closing the mobile menu after clicking on a link
-let [...link] = document.querySelectorAll("#mobMenu a");
-link.forEach((item) => {
-  item.addEventListener("click", function () {
-    el("#mobMenu").classList.remove("active");
-    el("body").classList.remove("no-scroll");
-  });
-});
+el('#mobMenu').addEventListener('click', (e) => {
+  if (e.target.id === 'mobMenu') return
+  e.target = menu()
+  e.target = scroll()
+})
 
 
