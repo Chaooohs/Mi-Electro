cd8d17e563b7af137c94e40c8d63a6f99baecde7
Chaooohs <01nikka01@gmail.com>
2023-07-25 20:19:00 +0300

    dsf

--- js/style.js
+++ js/style.js
@@ -5,29 +5,23 @@ const el = (selector) => document.querySelector(selector);
 
 el("#listBtn").onclick = () => el("#list").classList.toggle("hide-list");
 
-
 //  mobile menu
-const menu = function () {
-  el('#underLay').classList.toggle('active');
-  el('#mobMenu').classList.toggle('active');
-}
-
+const menu = () => el("#mobMenu").classList.toggle("active");
 const scroll = () => el("body").classList.toggle("no-scroll");
+const underline = () => el("#underLay").classList.toggle("under");
 
-el('#burgerBtn').addEventListener('click', menu);
-el('#burgerBtn').addEventListener('click', scroll);
+el("#burgerBtn").addEventListener("click", menu);
+el("#burgerBtn").addEventListener("click", scroll);
+el("#burgerBtn").addEventListener("click", underline);
 
-el('#underLay').addEventListener('click', function () {
-  menu()
-  scroll()
-})
-
+el("#underLay").addEventListener("click", menu);
+el("#underLay").addEventListener("click", scroll);
+el("#underLay").addEventListener("click", underline);
 
 // closing the mobile menu after clicking on a link
-el('#mobMenu').addEventListener('click', (e) => {
-  if (e.target.id === 'mobMenu') return
-  e.target = menu()
-  e.target = scroll()
-})
-
-
+el("#mobMenu").addEventListener("click", (e) => {
+  if (e.target.id === "mobMenu") return;
+  e.target = menu();
+  e.target = scroll();
+  e.target = underline();
+});
